e6e6b6a691f440db209312607694c8b6e59d3528
Umesh Chaurasiya <chaurasiyaumesh9@gmail.com>
2016-05-08 19:21:46 +0530

    commit-2

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -2,5 +2,8 @@
 angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
     $scope.tagline = 'Nothing beats a pocket protector!';
-
+	
+	$scope.addNew = function( nerd ){
+		
+	}
 });
